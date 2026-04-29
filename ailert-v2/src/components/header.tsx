50c7a3222c933c4c5eab884d8d73834f442884a7
Fernanda <febeltraoleite@gmail.com>
2026-04-29 19:49:04 -0300

    feat: estrutura inicial do projeto e componente Header

---
+++ ailert-v2/src/components/header.tsx
@@ -0,0 +1,12 @@
+function Headder(){
+    return (
+        <header>
+            <div className="logo">
+                <div className="logo-dot">
+                    <span>AIlert</span>
+                </div>
+                <button>Sair rápido</button>
+            </div>
+        </header>
+    )
+}
